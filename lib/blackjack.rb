b9ec04de2ad24d79b2f2c8f0b5b28d68b951131f
Samuel Lesser <ineedmoreplates@gmail.com>
2020-06-04 00:56:40 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,7 +57,16 @@ end
 
 def runner
   welcome
+  total = initial_round
+
+until total > 21
     display_card_total(initial_round)
+      total = hit?(total)
+  
+    end
+  end_game(total)
+end
     
+  runner
 end
     
